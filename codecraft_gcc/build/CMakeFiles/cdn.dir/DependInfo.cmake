00898e2875b8ef2cb9fbe2de49075a417b146e91
hhr <1016755439@qq.com>
2017-03-17 17:43:39 +0800

    all files

---
+++ codecraft_gcc/build/CMakeFiles/cdn.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nuaa/HUAWEI_Code_Craft_2017/codecraft_gcc/cdn/cdn.cpp" "/home/nuaa/HUAWEI_Code_Craft_2017/codecraft_gcc/build/CMakeFiles/cdn.dir/cdn.cpp.o"
+  "/home/nuaa/HUAWEI_Code_Craft_2017/codecraft_gcc/cdn/deploy.cpp" "/home/nuaa/HUAWEI_Code_Craft_2017/codecraft_gcc/build/CMakeFiles/cdn.dir/deploy.cpp.o"
+  "/home/nuaa/HUAWEI_Code_Craft_2017/codecraft_gcc/cdn/io.cpp" "/home/nuaa/HUAWEI_Code_Craft_2017/codecraft_gcc/build/CMakeFiles/cdn.dir/io.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/nuaa/HUAWEI_Code_Craft_2017/codecraft_gcc/cdn/lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ codecraft_gcc/build/CMakeFiles/cdn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cdn.dir/io.cpp.o"
+  "CMakeFiles/cdn.dir/cdn.cpp.o"
+  "CMakeFiles/cdn.dir/deploy.cpp.o"
+  "/home/nuaa/HUAWEI_Code_Craft_2017/codecraft_gcc/bin/cdn.pdb"
+  "/home/nuaa/HUAWEI_Code_Craft_2017/codecraft_gcc/bin/cdn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cdn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
